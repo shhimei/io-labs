6689f3379371220deff52a3818a7f9434436cdb2
shimei <polina.pavlova19.10@gmail.com>
2023-04-17 17:01:53 +0300

    initial lab1 commit

---
+++ lab1/char_mod.c
@@ -0,0 +1,10 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Polina Pavlova");
+MODULE_DESCRIPTION("io-lab1 module");
+
+
